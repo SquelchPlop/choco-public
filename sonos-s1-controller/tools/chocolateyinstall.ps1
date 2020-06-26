38e6881851ab4304e3881cd9d043148baf78e1cc
SquelchPlop <matthewhadley16@hotmail.com>
2020-06-26 22:47:15 +0100

    feat: initial checkin of Sonos S1 Controller

---
+++ sonos-s1-controller/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = "Stop"; # stop on all errors
+#$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  fileType        = "EXE"
+  url             = "https://update-software.sonos.com/software/pc/dcr/SonosDesktopController112.exe"
+  checksum        = "0662D22698D1B798E3A817960B45BF614160518EF59C958EFA2272CF4583611C"
+  checksumType    = "sha256"
+  silentArgs     = '/s /v"/qn"'
+  validExitCodes	= @(0, 3010)
+  softwareName    = "Sonos Controller"
+}
+
+Install-ChocolateyPackage @packageArgs
